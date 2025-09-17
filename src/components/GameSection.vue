b3f8b122dfc605c7a8624e2d5984acada659a07d
Gibran <gibrandaffa92@gmail.com>
2025-09-17 17:34:17 +0700

    dev(page:game): Header icon animation

--- src/components/GameSection.vue
+++ src/components/GameSection.vue
@@ -1,14 +1,24 @@
 <script setup>
 import { Icon } from '@iconify/vue';
+
+defineProps(['isOpen'])
 defineEmits(['toggle'])
 </script>
 
 <template>
    <section class="rounded-lg bg-zinc-800 overflow-hidden transition-all duration-400">
-      <h1 class="text-xl font-mozilla-headline font-bold flex items-center px-3 py-2 gap-2">
-         <Icon icon="solar:alt-arrow-down-bold" class="text-2xl" @click="$emit('toggle')" />
+
+      <h1 class="text-xl font-mozilla-headline font-bold flex items-center px-3 py-2 gap-2 overflow-visible justify-start">
+         <Icon
+            icon="solar:alt-arrow-down-bold"
+            class="text-2xl transition-all duration-400"
+            :class="{'-rotate-90': !isOpen}"
+            @click="$emit('toggle')"
+         />
          <slot name="title"/>
       </h1>
+
       <slot/>
+
    </section>
 </template>
--- src/contents/GameView.vue
+++ src/contents/GameView.vue
@@ -1,35 +1,79 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { reactive } from 'vue';
 import GameSection from '@/components/GameSection.vue';
 
-const keyOpen = ref(false)
-const $key = computed(() => ({
-   // --spacing(11) + gap-3
-   'h-[calc(100%---spacing(14))]': keyOpen.value,
-   'h-11': !keyOpen.value
-}))
-const explanationOpen = ref(true)
-const $explanation = computed(() => ({
-   // --spacing(11) + gap-3
-   'h-[calc(100%---spacing(14))]': explanationOpen.value,
-   'h-11': !explanationOpen.value
-}))
+// =============================================================================
+
+const isOpen = reactive({
+   game: true,
+   key: true,
+   explanation: true
+})
+
+const $leftSide = {
+   open: 'h-[calc(100%---spacing(14))]',
+   closed: 'h-11'
+}
+const $rightSide = {
+   open: '',
+   closed: ''
+}
+
+// =============================================================================
+
+/**
+ * Change section state on `isOpen` reactive variable.
+ * 
+ * @param section - Section name.
+ */
+function toggleState(section) {
+   isOpen[section] = !isOpen[section]
+}
+
+/**
+ * Render a specific class to a section according to its side.
+ * 
+ * @param side - Right or left side on the website view.
+ * @param section - Section name.
+ */
+function renderClasses(side, section) {
+   let status = isOpen[section] ? 'open' : 'closed'
+   let selectedSide = {left: $leftSide, right: $rightSide}[side]
+   return selectedSide[status]
+}
 </script>
 
 <template>
    <div class="p-3 h-[100dvh] flex gap-3">
       <div class="flex flex-col float-left w-[50%] h-full max-h-full gap-3">
-         <GameSection class="" :class="$key" @toggle="keyOpen=!keyOpen">
+
+         <GameSection
+            :class="renderClasses('left', 'key')"
+            :isOpen="isOpen.key"
+            @toggle="toggleState('key')"
+         >
             <template #title>Key</template>
             <p class="px-11 py-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et eos vitae possimus cumque deleniti sapiente eligendi.</p>
          </GameSection>
-         <GameSection class="" :class="$explanation" @toggle="explanationOpen=!explanationOpen">
+
+         <GameSection
+            :class="renderClasses('left', 'explanation')"
+            :isOpen="isOpen.explanation"
+            @toggle="toggleState('explanation')"
+         >
             <template #title>Explanation</template>
             <p class="px-11 py-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et eos vitae possimus cumque deleniti sapiente eligendi.</p>
          </GameSection>
+
       </div>
-      <GameSection class="w-[50%] float-right">
+
+      <GameSection
+         class="w-[50%] float-right"
+         :class="renderClasses('right', 'game')"
+         :isOpen="isOpen.explanation"
+      >
          <template #title>Game</template>
       </GameSection>
+
    </div>
 </template>
