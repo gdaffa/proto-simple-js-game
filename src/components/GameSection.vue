971983bccbedfdbc76b4d9a95e89c503f6f0ab1c
Gibran <gibrandaffa92@gmail.com>
2025-09-19 18:27:50 +0700

    style(page:game): Format code with prettier

--- src/components/GameSection.vue
+++ src/components/GameSection.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Icon } from '@iconify/vue';
+import { Icon } from '@iconify/vue'
 
 defineProps(['isOpen'])
 defineEmits(['toggle'])
@@ -7,24 +7,19 @@ defineEmits(['toggle'])
 
 <template>
    <section class="rounded-lg bg-zinc-800 overflow-hidden transition-all duration-600">
-
       <div class="grid grid-cols-[min-content_1fr] items-center">
-         <button
-            class="w-11 aspect-square grid place-items-center"
-            @click="$emit('toggle')"
-         >
+         <button class="w-11 aspect-square grid place-items-center" @click="$emit('toggle')">
             <Icon
                icon="solar:alt-arrow-down-bold"
                class="text-2xl transition-all duration-400"
-               :class="{'-rotate-90': !isOpen}"
+               :class="{ '-rotate-90': !isOpen }"
             />
          </button>
          <h1 class="text-xl font-mozilla-headline font-bold py-2">
-            <slot name="title"/>
+            <slot name="title" />
          </h1>
       </div>
 
-      <slot/>
-
+      <slot />
    </section>
 </template>
--- src/contents/GameView.vue
+++ src/contents/GameView.vue
@@ -1,36 +1,20 @@
 <script setup>
-import { reactive, computed } from 'vue';
-import GameSection from '@/components/GameSection.vue';
+import { reactive, computed } from 'vue'
+import GameSection from '@/components/GameSection.vue'
 
 // =============================================================================
 
 const isOpen = reactive({
    game: true,
    key: true,
-   explanation: true
+   explanation: true,
 })
-
-const $leftSection = {
-   open: 'h-dvh lg:h-[calc(100%---spacing(14))]',
-   closed: 'h-11'
-}
-
-const $leftSide = computed(() => ({
-   'w-full lg:w-full': !isOpen.game,
-   'w-full lg:w-[50%]': isOpen.game && (isOpen.key || isOpen.explanation),
-   'w-full lg:w-11': !isOpen.key && !isOpen.explanation
-}))
-const $rightSide = computed(() => ({
-   'w-full h-11 lg:w-11 lg:h-11': !isOpen.game,
-   'w-full h-dvh lg:w-[50%] lg:h-full': isOpen.game && (isOpen.key || isOpen.explanation),
-   'w-full h-dvh lg:w-full  lg:h-full': !isOpen.key && !isOpen.explanation
-}))
 
 // =============================================================================
 
 /**
  * Change left side section state in `isOpen` variable.
- * 
+ *
  * @param section - Section name, either `key` or `explanation`.
  */
 function toggleLeftSection(section) {
@@ -56,21 +40,35 @@ function toggleRightSection() {
 
 /**
  * Get a class that is already determined for left section side.
- * 
+ *
  * @param section - Section name, either `key` or `explanation`.
  */
 function getLeftSectionClass(section) {
    return $leftSection[isOpen[section] ? 'open' : 'closed']
 }
+
+// =============================================================================
+
+const $leftSection = {
+   open: 'h-dvh lg:h-[calc(100%---spacing(14))]',
+   closed: 'h-11',
+}
+
+const $leftSide = computed(() => ({
+   'w-full lg:w-full': !isOpen.game,
+   'w-full lg:w-[50%]': isOpen.game && (isOpen.key || isOpen.explanation),
+   'w-full lg:w-11': !isOpen.key && !isOpen.explanation,
+}))
+const $rightSide = computed(() => ({
+   'w-full h-11  lg:w-11    lg:h-11': !isOpen.game,
+   'w-full h-dvh lg:w-[50%] lg:h-full': isOpen.game && (isOpen.key || isOpen.explanation),
+   'w-full h-dvh lg:w-full  lg:h-full': !isOpen.key && !isOpen.explanation,
+}))
 </script>
 
 <template>
    <div class="p-3 grid grid-rows-[min-content_min-content] lg:h-dvh lg:flex mt-15 md:mt-0 gap-3">
-      <div
-         class="flex flex-col gap-3 row-2 transition-all duration-600"
-         :class="$leftSide"
-      >
-
+      <div class="flex flex-col gap-3 row-2 transition-all duration-600" :class="$leftSide">
          <GameSection
             class="hidden lg:block"
             :class="getLeftSectionClass('key')"
@@ -78,29 +76,27 @@ function getLeftSectionClass(section) {
             @toggle="toggleLeftSection('key')"
          >
             <template #title>Key</template>
-            <p class="px-11 py-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et eos vitae possimus cumque deleniti sapiente eligendi.</p>
+            <p class="px-11 py-3">
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et
+               eos vitae possimus cumque deleniti sapiente eligendi.
+            </p>
          </GameSection>
-
          <GameSection
             :class="getLeftSectionClass('explanation')"
             :isOpen="isOpen.explanation"
             @toggle="toggleLeftSection('explanation')"
          >
             <template #title>Explanation</template>
-            <p class="px-11 py-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et eos vitae possimus cumque deleniti sapiente eligendi.</p>
+            <p class="px-11 py-3">
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non magni consectetur ad et
+               eos vitae possimus cumque deleniti sapiente eligendi.
+            </p>
          </GameSection>
-
       </div>
       <div class="row-1 transition-all duration-600" :class="$rightSide">
-
-         <GameSection
-            class="h-full"
-            :isOpen="isOpen.game"
-            @toggle="toggleRightSection"
-         >
+         <GameSection class="h-full" :isOpen="isOpen.game" @toggle="toggleRightSection">
             <template #title>Game</template>
          </GameSection>
-
       </div>
    </div>
 </template>
